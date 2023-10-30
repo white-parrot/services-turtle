6d20ca2bf3bca511a690d0f4d5c1e2348205ea4e
white-parrot <everydaycodestory@gmail.com>
2023-10-30 18:03:16 +0530

    EXPENSE-TRACKER 301020231802 : Service Skeleton Initialised

---
+++ RuntimeTurtle-ExpensesTracker/src/main/java/com/runtimeturtle/expensestracker/RuntimeTurtleExpensesTracker/RuntimeTurtleExpensesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.runtimeturtle.expensestracker.RuntimeTurtleExpensesTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntimeTurtleExpensesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntimeTurtleExpensesTrackerApplication.class, args);
+	}
+
+}
